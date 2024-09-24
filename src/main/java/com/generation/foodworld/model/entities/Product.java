ecc609d43ba97d9e4f23dd23ea53b254fb304734
Difroy <unifroy@gmail.com>
2024-09-24 15:46:41 +0200

    Create Product.java

---
+++ src/main/java/com/generation/foodworld/model/entities/Product.java
@@ -0,0 +1,120 @@
+package com.generation.foodworld.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int carbs;
+    private int proteins;
+    private int fats;
+    private double price;
+    private boolean vegan;
+    private String description;
+    private String ingredients;
+    private String image;
+
+
+    public Product() {}
+    
+    public Product(int id, String name, int carbs, int proteins, int fats, double price, boolean vegan, String description, String ingredients, String image) {
+        this.id = id;
+        this.name = name;
+        this.carbs = carbs;
+        this.proteins = proteins;
+        this.fats = fats;
+        this.price = price;
+        this.vegan = vegan;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.image = image;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(int carbs) {
+        this.carbs = carbs;
+    }
+
+    public int getProteins() {
+        return proteins;
+    }
+
+    public void setProteins(int proteins) {
+        this.proteins = proteins;
+    }
+
+    public int getFats() {
+        return fats;
+    }
+
+    public void setFats(int fats) {
+        this.fats = fats;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isVegan() {
+        return vegan;
+    }
+
+    public void setVegan(boolean vegan) {
+        this.vegan = vegan;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
