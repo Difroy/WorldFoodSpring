5e6b36853f5ca0800135b7c0e91113f7477a53bb
Difroy <unifroy@gmail.com>
2024-09-24 16:22:33 +0200

    Update ProductAPI.java

--- src/main/java/com/generation/foodworld/api/ProductAPI.java
+++ src/main/java/com/generation/foodworld/api/ProductAPI.java
@@ -1,10 +1,11 @@
 package com.generation.foodworld.api;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
