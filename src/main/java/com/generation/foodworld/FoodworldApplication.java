679aa247fb2d3565d044cd85ac26e293be15b4c7
Difroy <unifroy@gmail.com>
2024-09-24 15:44:48 +0200

    Update

---
+++ src/main/java/com/generation/foodworld/FoodworldApplication.java
@@ -0,0 +1,13 @@
+package com.generation.foodworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/foodworld/FoodworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.foodworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
